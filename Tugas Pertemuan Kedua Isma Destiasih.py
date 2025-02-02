9d29ff6f6eb569d49fe787f3036d90ceb8644cda
ismades <ismadestiasih@gmail.com>
2025-02-02 10:16:01 +0700

    Upload Python calculator project
    Just a small Python project I worked on while learning the basics of programming. 
    This script can:
    -Do basic math (addition, subtraction, multiplication, division)  
    -Compare two numbers  
    -Work with lists (storing and displaying data)  
    
    A simple but fun way to practice Python!   
    Excited to keep learning and building more projects.

---
+++ Tugas Pertemuan Kedua Isma Destiasih.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 20:22:08 2024
+
+@author: user
+"""
+
+A = int(input("masukkan angka pertama:"))
+B = int(input("masukkan angka kedua:"))
+Penjumlahan = A+B
+Pengurangan = A-B
+Perkalian = A*B
+Pembagian = A/B
+print("")
+print("=========== Nomor 1 ==========")
+print("Hasil penjumlahan:", Penjumlahan)
+print("Hasil pengurangan:", Pengurangan)
+print("Hasil perkalian:", Perkalian)
+print("Hasil pembagian:", Pembagian)
+print("")
+print("=========== Nomor 2 ==========")
+print("Hasil dari perbandingan A>B", A>B)
+print("Hasil dari perbandingan A<B", A<B)
+print("Hasil dari perbandingan A==B", A==B)
+print("Hasil dari perbandingan A!=B", A!=B)
+print("Hasil dari perbandingan A>=B", A>=B)
+print("Hasil dari perbandingan A<=B", A<=B)
+print("")
+print("=========== Nomor 3 ==========")
+Buah = ["Pisang", "Semangka", "Jeruk", "Alpukat", "Sirsak"]
+Sayuran =["Pakcoi", "Bayam", "Kangkung", "Sawi", "Terong", "Kelor", "Pare"]
+Makanan_Kesukaan =["Ayam Geprek", "Takoyaki", "Nasi Goreng"]
+print(Buah)
+print(Sayuran)
+print(Makanan_Kesukaan)
+print("")
+print("=========== Nomor 4 ==========")
+print(Buah[1])#buah kedua
+print(Sayuran[6])#sayuran ketujuh
+print(Makanan_Kesukaan[0])#makanan pertama
